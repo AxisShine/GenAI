ff6cf628c5d5f3c723c3dbe445462808cd016e37
AxisShine <alicejsedgwick2004@gmail.com>
2025-03-22 17:13:40 -0400

    Updating the upload on the learning page

--- edubridge/src/Components/Learning.jsx
+++ edubridge/src/Components/Learning.jsx
@@ -1,5 +1,12 @@
+import Upload from "../Components/Upload"; // Import the Upload component
+
 function Learning() {
-  return <></>;
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Learning Mode</h1>
+      <Upload /> {/* Display the Upload component here */}
+    </div>
+  );
 }
 
 export default Learning;
