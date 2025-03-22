fe5c01062a263deacc56ca4b08fc4c9a0adde26d
Krixsick <linusgao123@gmail.com>
2025-03-22 15:10:32 -0400

    Dashboard completed

--- edubridge/src/App.jsx
+++ edubridge/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Components/Home.jsx";
 import Questions from "./Components/Questions.jsx";
 import "./App.css";
+import DashBoard from "./Components/DashBoard.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/questions" element={<Questions></Questions>}></Route>
+          <Route path="/dashboard" element={<DashBoard></DashBoard>}></Route>
         </Routes>
       </Router>
     </>
